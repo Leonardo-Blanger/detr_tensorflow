2df4ff46d2dce82c6872c2f06c823f293526200a
Leonardo Blanger <leonardoblanger@gmail.com>
2020-06-29 17:31:09 -0300

    Added a custom implementation for the Linear/Dense layer, that allows loading PyTorch converted weights

--- custom_layers.py
+++ custom_layers.py
@@ -26,3 +26,29 @@ class FrozenBatchNorm2D(tf.keras.layers.Layer):
 
     def compute_output_shape(self, input_shape):
         return input_shape
+
+
+class Linear(tf.keras.layers.Layer):
+    '''
+    Use this custom layer instead of tf.keras.layers.Dense to allow
+    loading converted PyTorch Dense weights that have shape (output_dim, input_dim)
+    '''
+    def __init__(self, output_dim, **kwargs):
+        super().__init__(**kwargs)
+        self.output_dim = output_dim
+
+
+    def build(self, input_shape):
+        self.kernel = self.add_weight(name='kernel',
+                                      shape=[self.output_dim, input_shape[-1]],
+                                      initializer='zeros', trainable=True)
+        self.bias = self.add_weight(name='bias',
+                                    shape=[self.output_dim],
+                                    initializer='zeros', trainable=True)
+
+    def call(self, x):
+        return tf.matmul(x, self.kernel, transpose_b=True) + self.bias
+
+
+    def compute_output_shape(self, input_shape):
+        return input_shape.as_list()[:-1] + [self.output_dim]
