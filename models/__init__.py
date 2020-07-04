1e557f564ca722bb6fbfbfbec36d89836e15b64a
Leonardo Blanger <leonardoblanger@gmail.com>
2020-07-03 23:45:01 -0300

    Added default number of classes = 91, to match the weights pretrained on the COCO dataset.

--- models/__init__.py
+++ models/__init__.py
@@ -1,14 +1,14 @@
 from .detr import DETR
 
 
-def build_detr_resnet50(num_classes, num_queries=100):
+def build_detr_resnet50(num_classes=91, num_queries=100):
     from .backbone import ResNet50Backbone
     return DETR(num_classes=num_classes,
                 num_queries=num_queries,
                 backbone=ResNet50Backbone(name='backbone/0/body'))
 
 
-def build_detr_resnet50_dc5(num_classes, num_queries=100):
+def build_detr_resnet50_dc5(num_classes=91, num_queries=100):
     from .backbone import ResNet50Backbone
     return DETR(num_classes=num_classes,
                 num_queries=num_queries,
@@ -17,14 +17,14 @@ def build_detr_resnet50_dc5(num_classes, num_queries=100):
                     name='backbone/0/body'))
 
 
-def build_detr_resnet101(num_classes, num_queries=100):
+def build_detr_resnet101(num_classes=91, num_queries=100):
     from .backbone import ResNet101Backbone
     return DETR(num_classes=num_classes,
                 num_queries=num_queries,
                 backbone=ResNet101Backbone(name='backbone/0/body'))
 
 
-def build_detr_resnet101_dc5(num_classes, num_queries=100):
+def build_detr_resnet101_dc5(num_classes=91, num_queries=100):
     from .backbone import ResNet101Backbone
     return DETR(num_classes=num_classes,
                 num_queries=num_queries,
