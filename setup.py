02832e272de182c4d359f9fc1c846eea6f166b6b
Leonardo Blanger <leonardoblanger@gmail.com>
2021-05-22 14:35:24 -0300

    Setup file.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='detr_tensorflow',
+    packages=find_packages(include=['detr_tensorflow']),
+    version='0.1.0',
+    description='DETR Object Detection architecture in Tensorflow',
+    author='Leonardo Blanger',
+)
